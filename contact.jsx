c3831739648503e073740ae6331d388e5ba5b5f3
Idris Abdu <idrismussa700@gmail.com>
2025-11-29 11:13:49 +0300

    portfolio

---
+++ contact.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import  'portfolio2.css'
+import 'portfolio2.html'
+
+const portfolio2 = () => {
+    return(
+        <header>
+            <h2>id.coder</h2>
+            <nav>
+                <ul>
+                    <li><a href="#home">home</a></li>
+                    <li><a href="#about">about</a></li>
+                    <li><a href="#services">services</a></li>
+                    <li><a href="#skills">skills</a></li>
+                    <li><a href="#project">projects</a></li>
+                    <li><a href="#contact">contact</a> </li>
+                </ul>
+            </nav>
+        </header>
+    
+    )
+}
+ portfolio2()
+    
---
+++ portfolio.js
@@ -0,0 +1,51 @@
+'use strict';
+
+    const preloader = document.querySelector("[data-preload]");
+
+    window.addEventListener("load", function() {
+        preloader.classList.add("loaded");
+        document.body.classList.add("loaded");
+    });
+
+    const addEventOnElements = function(elements, eventType, callback) {
+        for(let i = 0, len = elements.length; i < len; i++){
+            elements[i].addEventListener(eventType, callback);
+        }
+    };
+
+    
+    document.querySelectorAll('.input-box').forEach(input => {
+        const toggleFilled = () => {
+            if(input.value != ''){
+                input.classList.add('filled');
+            }else{
+                input.classList.remove('filled')
+            }
+        }
+
+        toggleFilled();
+
+        input.addEventListener('input', toggleFilled);
+        input.addEventListener('change', toggleFilled);
+        input.addEventListener('blur', toggleFilled);
+
+    });
+
+
+
+    let btn = document.getElementById('btn')
+    window.addEventListener('scroll',function(){
+        if(window.scrollY >= 600){
+            btn.style.display = 'block';
+        }else{
+            btn.style.display = 'none';
+        }
+    })
+
+    btn.addEventListener('click', () => {
+        scroll({
+            left:0,
+            top:0,
+            behavior: 'smooth'
+        })
+    })
